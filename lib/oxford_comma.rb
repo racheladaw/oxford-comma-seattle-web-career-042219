237cd489c3daf25faba1011ccba41806c37b5845
Rachel <williamsrachelada@gmail.com>
2019-03-11 13:56:03 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    last_item = array.pop
+    new_string = array.join(", ")
+    new_string + ", and #{last_item}"
+  end
 end
